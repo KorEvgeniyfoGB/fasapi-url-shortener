ea137062da7d64765ce777045ca444abe9cc40f8
Evgeniy <you@example.com>
2025-04-16 21:52:08 +0300

    amended

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -6,5 +6,10 @@ app = FastAPI(
 )
 
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+
 # if __name__ == "__main__":
 #     uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
