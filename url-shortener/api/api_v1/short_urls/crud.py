7571f620c22ba43f82bff9f6acb9ee0e97f80f8b
Evgeniy <Bagz.bann4-l011a.Bann4@yandex.by>
2025-04-23 20:31:32 +0300

    create file storage

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel, AnyHttpUrl
+from pydantic import (
+    BaseModel,
+    ValidationError,
+)
 
 from shemas.shorter_url import (
     ShortUrl,
@@ -6,6 +9,8 @@ from shemas.shorter_url import (
     ShortUrlUpdate,
     ShortUrlUpdatePartial,
 )
+
+from core.config import URL_SHORT_STORAGE_FILE
 
 # SHORT_URLS = [
 #     ShortUrl(
@@ -22,6 +27,15 @@ from shemas.shorter_url import (
 class ShortUrlStorage(BaseModel):
     slug_to_short_url: dict[str, ShortUrl] = {}
 
+    def safe_state(self) -> None:
+        URL_SHORT_STORAGE_FILE.write_text(self.model_dump_json(indent=2))
+
+    @classmethod
+    def from_state(cls) -> "ShortUrlStorage":
+        if not URL_SHORT_STORAGE_FILE.exists():
+            return ShortUrlStorage()
+        return cls.model_validate_json(URL_SHORT_STORAGE_FILE.read_text())
+
     def get(self) -> list[ShortUrl]:
         return list(self.slug_to_short_url.values())
 
@@ -31,10 +45,12 @@ class ShortUrlStorage(BaseModel):
     def create(self, short_url_in: ShortUrlCreate) -> ShortUrl:
         short_url = ShortUrl(**short_url_in.model_dump())
         self.slug_to_short_url[short_url.slug] = short_url
+        self.safe_state()
         return short_url
 
     def delete_by_slug(self, slug: str) -> None:
         self.slug_to_short_url.pop(slug, None)
+        self.safe_state()
 
     def delete(self, short_url: ShortUrl) -> None:
         self.delete_by_slug(slug=short_url.slug)
@@ -46,6 +62,7 @@ class ShortUrlStorage(BaseModel):
     ) -> ShortUrl:
         for field_name, value in short_url_in:
             setattr(short_url, field_name, value)
+        self.safe_state()
         return short_url
 
     def update_partial(
@@ -55,21 +72,27 @@ class ShortUrlStorage(BaseModel):
     ) -> ShortUrl:
         for field_name, values in short_url_in.model_dump(exclude_unset=True).items():
             setattr(short_url, field_name, values)
+        self.safe_state()
         return short_url
 
 
-storage = ShortUrlStorage()
+try:
+    storage = ShortUrlStorage.from_state()
+except ValidationError:
+    storage = ShortUrlStorage()
+    storage.safe_state()
 
-storage.create(
-    ShortUrlCreate(
-        target_url=AnyHttpUrl("https://www.example.com"),
-        slug="example",
-    )
-)
 
-storage.create(
-    ShortUrlCreate(
-        target_url=AnyHttpUrl("https://www.google.com"),
-        slug="search",
-    )
-)
+# storage.create(
+#     ShortUrlCreate(
+#         target_url=AnyHttpUrl("https://www.example.com"),
+#         slug="example",
+#     )
+# )
+#
+# storage.create(
+#     ShortUrlCreate(
+#         target_url=AnyHttpUrl("https://www.google.com"),
+#         slug="search",
+#     )
+# )
---
+++ url-shortener/core/config.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+URL_SHORT_STORAGE_FILE = BASE_DIR / "short_urls.json"
